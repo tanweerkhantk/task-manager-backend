dac0daa63a5bcd041552d449169b8f56d8596efa
unknown <khantanweer656@gmail.com>
2026-03-16 15:12:46 +0530

    server

--- server.js
+++ server.js
@@ -42,7 +42,10 @@ const taskRoutes = require("./routes/taskRoutes")
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
+
 app.use(express.json())
 
 app.use("/api/auth", authRoutes) 
