ae1fc82db4bc37dcc1d2f4507afb8047dad2899e
unknown <khantanweer656@gmail.com>
2026-03-16 14:46:02 +0530

    server

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
+// const express = require('express')
+// const mongoose = require("mongoose")
+// const cors = require("cors")
+// require("dotenv").config()
+
+// const authRoutes = require("./routes/authRoutes")
+// const taskRoutes = require("./routes/taskRoutes")
+
+// const app = express()
+
+// app.use(cors())
+// app.use(express.json())
+
+// app.use("/api/auth", authRoutes, (req,res) => {
+//     res.send("Server")
+// }) 
+
+// app.use('/api/tasks', taskRoutes)
+
+// mongoose.connect(process.env.MONGO_DB_URL)
+// .then(() => console.log("MongoDb Connected"))
+// .catch(err => console.log(err))
+
+
+
+
+
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is started ${process.env.PORT}`);
+    
+// }) 
+
+
 const express = require('express')
 const mongoose = require("mongoose")
 const cors = require("cors")
@@ -11,15 +45,21 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/auth", authRoutes, (req,res) => {
-    res.send("Server")
-}) 
+app.use("/api/auth", authRoutes) 
 
 app.use('/api/tasks', taskRoutes)
+
+app.get("/", (req,res) => {
+    res.send("Backend API Working")
+})
 
 mongoose.connect(process.env.MONGO_DB_URL)
 .then(() => console.log("MongoDb Connected"))
 .catch(err => console.log(err))
+
+//Important for Varcel
+
+module.exports = app
 
 
 
