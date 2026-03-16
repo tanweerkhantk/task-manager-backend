651313f9c52b69877ccf2c77c9d0861e709740a3
unknown <khantanweer656@gmail.com>
2026-03-16 15:26:58 +0530

    server

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.use(cors({
         "https://task-manager-frontend-alpha-rust.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }))
 
